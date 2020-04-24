f050edb46c1e29a119a884dfed026288a2fc936c
user1 <user1@DESKTOP-448P4MH>
2020-04-24 11:12:55 +0900

    세번째 커밋

--- gitTest/src/com/kh/git/GitPrac.java
+++ gitTest/src/com/kh/git/GitPrac.java
@@ -1,9 +1,10 @@
 package com.kh.git;
 
-public class GitPrac {
+public class GitPrac { 
 	public static void main(String[] args) {
 		System.out.println("hello");
 		System.out.println("world");
 		System.out.println("thx");
+		// 잘 올라갔겠지?
 	}
 }
