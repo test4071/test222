d1a9d21b26bde15f3bddef4521b9d3030b4b54d2
user1 <user1@DESKTOP-448P4MH>
2020-04-24 11:12:00 +0900

    two

--- gitTest/src/com/kh/git/GitPrac.java
+++ gitTest/src/com/kh/git/GitPrac.java
@@ -3,5 +3,7 @@ package com.kh.git;
 public class GitPrac {
 	public static void main(String[] args) {
 		System.out.println("hello");
+		System.out.println("world");
+		System.out.println("thx");
 	}
 }
